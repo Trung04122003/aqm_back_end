8b7d1cff2b17dbfbd0208a3aad3aeba77680dfb9
Trung04122003 <lethaitrung03@gmail.com>
2025-05-22 19:26:37 +0700

    phase 1 - Project Kickstart

---
+++ aqm-back-end/src/main/java/com/commander/aqm/aqm_back_end/AqmBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.commander.aqm.aqm_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AqmBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AqmBackEndApplication.class, args);
+	}
+
+}
---
+++ aqm-back-end/src/test/java/com/commander/aqm/aqm_back_end/AqmBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commander.aqm.aqm_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AqmBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
